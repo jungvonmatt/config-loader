4b5e3a5157c9bef2611e92329467adee0da2327f
Ben Zörb <benjamin.zoerb@jvm.com>
2025-05-29 18:48:23 +0200

    adds sonar coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,13 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    reporters: [
+      "default",
+      ["vitest-sonar-reporter", { outputFile: "coverage/sonar-report.xml" }],
+    ],
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "lcov"],
       exclude: [
         "node_modules/",
         "dist/",
